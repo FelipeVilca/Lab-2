f4e508b4a230da6e55df901ea85c62ceebad8b46
Felipe Vilca <55534469+FelipeVilca@users.noreply.github.com>
2020-10-03 10:51:18 +0200

    Create Shape2DClass.cs

---
+++ Shape2DClass.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2
+{
+    public abstract class Shape2D : Shape
+    {
+        public abstract float Circumference { get; }
+    }
+}
